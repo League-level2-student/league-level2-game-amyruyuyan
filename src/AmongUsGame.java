6d51d8125e6ef8bc8a92716b90a238d1a692d337
amyruyuyan <41204932+amyruyuyan@users.noreply.github.com>
2020-11-08 14:28:59 -0800

    yuh

--- src/AmongUsGame.java
+++ src/AmongUsGame.java
@@ -2,7 +2,24 @@ import javax.swing.JFrame;
 
 public class AmongUsGame {
 
-	 
+	AmongUsGamePanel GamePanel; 
 	JFrame frame;
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 450;
 	
+	public static void main(String[] args){
+		AmongUsGame AmongUsGame = new AmongUsGame();
+		AmongUsGame.setup();
+	}
+	
+	void setup() {
+		frame = new JFrame();
+		GamePanel = new AmongUsGamePanel();
+		frame.add(GamePanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.addMouseMotionListener(GamePanel);
+		frame.addMouseListener(GamePanel);
+	}
 }
--- src/AmongUsGamePanel.java
+++ src/AmongUsGamePanel.java
@@ -1,4 +1,141 @@
 
-public class AmongUsGamePanel {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-}
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class AmongUsGamePanel extends JPanel implements MouseMotionListener, ActionListener, MouseListener {
+	Font titleFont;
+	Font secondFont;
+	Font thirdFont;
+	Font endFont;
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	int currentState;
+	Timer frameDraw;
+	boolean playIsGreen = false;
+	
+	AmongUsGamePanel(){
+		currentState = MENU;
+		frameDraw = new Timer(1000/60, this);
+		frameDraw.start();
+		titleFont = new Font("Courier New", Font.PLAIN, 96);
+		secondFont = new Font("Courier New", Font.PLAIN, 48);
+		thirdFont = new Font("Courier New", Font.PLAIN, 36);
+	}
+	
+		void drawMenuState(Graphics g){
+			g.setColor(Color.black);
+			g.fillRect(0, 0, AmongUsGame.WIDTH, AmongUsGame.HEIGHT);
+			
+			g.setFont(titleFont);
+			g.setColor(Color.white);
+			g.drawString("Among Us", 170, 100);
+			
+			g.setFont(secondFont);
+			g.setColor(Color.white);
+			g.drawString("Press 'Play' To Play", 100, 350);
+			
+			if (playIsGreen) {
+			g.setFont(secondFont);
+			g.setColor(Color.green);
+			g.drawString("Play", 303, 350);
+			}
+			
+			g.setFont(thirdFont);
+			g.setColor(Color.white);
+			g.drawString("Press 'How To Play' For Instructions", 5, 225);
+		}
+	
+		void drawGameState(Graphics g) {
+			
+		}
+		
+		void drawEndState(Graphics g) {
+			
+		}
+		
+		public void paintComponent(Graphics g){
+			if(currentState == MENU){
+			    drawMenuState(g);}		
+			else if(currentState == GAME) {
+				drawGameState(g);
+			}
+			else if(currentState == END){
+		    drawEndState(g);
+		}
+		}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		int mouseX = e.getX();
+		int mouseY = e.getY();
+		if (mouseX >288 && mouseX < 432 && mouseY > 344 && mouseY < 383) {
+			playIsGreen = true;
+		}
+		else {
+			playIsGreen = false;
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(currentState == MENU){
+		    //updateMenuState();
+		}else if(currentState == GAME){
+		    //updateGameState();
+		}else if(currentState == END){
+		    //updateEndState();
+		    
+		}
+		//System.out.println("action");
+		repaint();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("X " + e.getX());
+		System.out.println("Y " + e.getY());
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	} 
+	
+	}
